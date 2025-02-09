968092ee5cdb3db0a1ac46942c9f6292c6c2b5dd
Mansukh <mansukhk20@outlook.com>
2025-02-09 13:23:21 +0530

    Find Max From 2 number

---
+++ basic/TwoMax.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class TwoMax {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter value of A:-");
+        int a=sc.nextInt();
+        System.out.println("Enter value of B:-");
+        int b=sc.nextInt();
+        if(a>b){
+            System.out.println("A is greater than B");
+        }
+        else{
+            System.out.println("B is Greater than A");
+        }
+    }
+}
