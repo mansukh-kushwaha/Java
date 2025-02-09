a8ba1b8d964cadcaaab7b1d83405b10792778bca
Mansukh <mansukhk20@outlook.com>
2025-02-09 13:14:05 +0530

    Check number is greater than 10 or not

---
+++ basic/Max10.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Max10 {
+    public static void main(String[] agrs){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter number");
+        int n=sc.nextInt();
+        if(n>10){
+            System.out.println("number is greater than 10");
+        }
+    }
+}
