8694f3a795beb7066f53de292e7396565b299456
Mansukh <mansukhk20@outlook.com>
2025-02-10 12:42:32 +0530

    Find even number from 50 to 1000

---
+++ basic/Even3.java
@@ -0,0 +1,12 @@
+public class Even3 {
+    public static void main(String[] args) {
+        int n=50;
+        while(n<=1000){
+            if(n%2==0){
+                System.out.print(n+ " ");
+            }
+                n++;
+        }
+    }
+
+}
