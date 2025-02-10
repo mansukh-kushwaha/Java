d84edc918bc6155cc2931d436cbec88aac880109
Mansukh <mansukhk20@outlook.com>
2025-02-10 22:17:43 +0530

    Reverse Table Print

---
+++ basic/Table5.java
@@ -0,0 +1,11 @@
+public class Table5 {
+    public static void main(String[] args){
+        int table=0;
+        int i=10;
+        while(i>=1){
+            table=2*i;
+            System.out.println(table);
+            i--;
+        }
+    }
+}
