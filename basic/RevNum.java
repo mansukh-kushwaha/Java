84802dbc3dff2e5ba11ff6ad3c660e623ca7f615
Mansukh <mansukhk20@outlook.com>
2025-02-10 12:53:09 +0530

    Print reverse number from 100 to 1

---
+++ basic/RevNum.java
@@ -0,0 +1,9 @@
+public class RevNum {
+    public static void main(String[] args) {
+        int n=100;
+        while(n>=1){
+            System.out.print(n+ " ");
+            n--;
+        }
+    }
+}
