a7e1209db7758307d529ee186f4f972b6a9ee3e0
Mansukh <mansukhk20@outlook.com>
2025-02-10 12:36:06 +0530

    Sum of first 10 digit by while loop

---
+++ basic/AddWhile.java
@@ -0,0 +1,11 @@
+public class AddWhile {
+    public static void main(String[] args) {
+        int n = 1;
+        int sum=0;
+        while(n<=10){
+            sum+=n;
+            n++;
+        }
+        System.out.println("Sum Of First 10 Digit is : " + sum);
+    }
+}
