87ed2c2b2ac83e04ecedba9f574bc720abb88a2c
Mansukh <mansukhk20@outlook.com>
2025-02-10 13:08:21 +0530

    Print 50 to 500 by While loop

---
+++ basic/While2.java
@@ -0,0 +1,10 @@
+public class While2 {
+    public static void main(String[] args) {
+        int n=50;
+        while(n<=500){
+            System.out.print(n+ " ");
+            n++;
+        }
+    }
+
+}
