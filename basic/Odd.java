da43a84d9d2886ffe147613170c9a242d16b61f8
Mansukh <mansukhk20@outlook.com>
2025-02-10 12:48:05 +0530

    Find odd number from 1 to 100

---
+++ basic/Odd.java
@@ -0,0 +1,12 @@
+public class Odd {
+    public static void main(String[] args) {
+        int n=1;
+        while(n<=100){
+            if(n%2!=0){
+                System.out.print(n+ " ");
+            }
+                n++;
+        }
+    }
+
+}
