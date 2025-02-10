2526c82ae9052734a1aebc747c4ba04b231df4ec
Mansukh <mansukhk20@outlook.com>
2025-02-10 13:06:17 +0530

    Print 1 to 100 by While loop

---
+++ basic/While.java
@@ -0,0 +1,9 @@
+public class While {
+    public static void main(String[] args) {
+        int n=1;
+        while(n<=100){
+            System.out.print(n+ " ");
+            n++;
+        }
+    }
+}
