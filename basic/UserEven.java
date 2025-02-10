77719b948487adab52ab4fba36b211696e5ee262
Mansukh <mansukhk20@outlook.com>
2025-02-10 13:03:17 +0530

    Print even number from user input staring and ending point

---
+++ basic/UserEven.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class UserEven {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Staring point");
+        int a=sc.nextInt();
+        System.out.println("Enter Ending point");
+        int b=sc.nextInt();
+        while(a<=b){
+            if(a%2==0){
+                System.out.print(a+ " ");
+            }
+            a++;
+        }
+    }
+}
