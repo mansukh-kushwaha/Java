078a10e8236705ffb803668ceb7e3d4e0b65f586
Mansukh <mansukhk20@outlook.com>
2025-02-10 12:52:13 +0530

    Find Even number from 100 to 1

---
+++ basic/RevEven.java
@@ -0,0 +1,11 @@
+public class RevEven {
+    public static void main(String[] args) {
+        int n=100;
+        while(n>=1){
+            if(n%2==0){
+                System.out.print(n+ " ");
+            }
+            n--;
+        }
+    }
+}
