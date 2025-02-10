71398dddb1956eeec8d5be72c3d646d599c5d2a2
Mansukh <mansukhk20@outlook.com>
2025-02-10 12:41:54 +0530

    Find even number from 1 to 100

---
+++ basic/Even2.java
@@ -0,0 +1,11 @@
+public class Even2 {
+    public static void main(String[] args) {
+        int n=1;
+        while(n<=100){
+            if(n%2==0){
+                System.out.print(n+ " ");
+            }
+                n++;
+        }
+    }
+}
