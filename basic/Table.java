541de4ba009c47d7184b04973566099f6187c1b0
Mansukh <mansukhk20@outlook.com>
2025-02-10 13:00:05 +0530

    Print table

---
+++ basic/Table.java
@@ -0,0 +1,11 @@
+public class Table {
+    public static void main(String[] args){
+        int table=0;
+        int i=1;
+        while(i<=10){
+            table=2*i;
+            System.out.println(table);
+            i++;
+        }
+    }
+}
