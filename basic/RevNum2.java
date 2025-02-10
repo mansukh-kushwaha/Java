dd8fc67767c89afb01af1382c674bfbf43f410ff
Mansukh <mansukhk20@outlook.com>
2025-02-10 12:54:07 +0530

    Print reverse number from 500 to 50

---
+++ basic/RevNum2.java
@@ -0,0 +1,9 @@
+public class RevNum2 {
+    public static void main(String[] args) {
+        int n=500;
+        while(n>=50){
+            System.out.print(n+ " ");
+            n--;
+        }
+    }
+}
