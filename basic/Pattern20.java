0c435cd528f3fe899d460f121a7c992493896ad3
Mansukh <mansukhk20@outlook.com>
2025-02-11 15:34:11 +0530

    Basic Pattern

---
+++ basic/Pattern20.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Pattern20 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter row");
+        int n=sc.nextInt();
+        int x=2;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                System.out.print(x+" ");
+                x+=2;
+            }
+        System.out.println();
+        }
+    }
+}
