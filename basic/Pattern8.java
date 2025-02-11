7e61d107b280d6cf4a4eaae5c88da8e9ab741888
Mansukh <mansukhk20@outlook.com>
2025-02-11 13:51:40 +0530

    Basic Pattern

---
+++ basic/Pattern8.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Pattern8 {
+        public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the row:-");
+        int row=sc.nextInt();
+        for(int i=1;i<=row;i++){
+            for(int s=1;s<i;s++){
+                System.out.print(" ");
+            }
+            for(int j=row;j>=i;j--){
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+    }
+}
