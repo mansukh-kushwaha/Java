91b079534df33734f66eb68ca47a0bbb9e9edbe8
Mansukh <mansukhk20@outlook.com>
2025-02-11 22:44:29 +0530

    Basic Pattern

---
+++ basic/Pattern36.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Pattern36 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter row:-");
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(i%2);
+            }
+            System.out.println();
+        }
+    }
+}
+/*
+Output:-
+1
+00 
+111
+0000 
+11111
+ */
