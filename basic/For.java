a3088aca05499b1d0547a6c7a6dbade0eae14836
Mansukh <mansukhk20@outlook.com>
2025-02-14 12:58:09 +0530

    Basic For loop

---
+++ basic/For.java
@@ -0,0 +1,13 @@
+// Write a program to print numbers from 1 to 10.
+import java.util.Scanner;
+
+public class For {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number ");
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            System.out.println(i);
+        }
+    }
+}
