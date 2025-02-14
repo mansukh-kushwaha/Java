3b9e0d6463aa64a4f43559bd8928e85ff6030167
Mansukh <mansukhk20@outlook.com>
2025-02-14 12:58:56 +0530

    Basic For loop

---
+++ basic/For1.java
@@ -0,0 +1,8 @@
+// 1--->100 number print
+public class For1 {
+    public static void main(String[] args) {
+        for(int i=1;i<=100;i++){
+            System.out.println(i);
+        }
+    }
+}
