5eaba2dfdf3482d6f7321138b4f860fd239ad050
Mansukh <mansukhk20@outlook.com>
2025-02-14 12:55:59 +0530

    Check number is divisible by 5 and 11 (for loop)

---
+++ basic/Div8.java
@@ -0,0 +1,9 @@
+public class Div8 {
+    public static void main(String[] args) {
+        for(int i=1;i<=100;i++){
+            if(i%5==0 && i%11==0){
+                System.out.print(i+ " ");
+            }
+        }
+    }
+}
