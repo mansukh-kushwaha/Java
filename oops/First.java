c85f06c423bbc66e17d1b156f341bd7e67cda8a0
Mansukh <mansukhk20@outlook.com>
2025-03-18 13:13:08 +0530

    First code of OOPs

---
+++ oops/First.java
@@ -0,0 +1,10 @@
+public class First {
+    int x = 5;
+  
+    public static void main(String[] args) {
+      First myObj1 = new First();  // Object 1
+      First myObj2 = new First();  // Object 2
+      System.out.println(myObj1.x);
+      System.out.println(myObj2.x);
+    }
+  }
